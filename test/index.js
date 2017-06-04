b69214aec5f4461f4c2af02667c4746ad4210b81
Luke Edwards <luke.edwards05@gmail.com>
2017-06-03 17:46:35 -0700

    add tests

---
+++ test/index.js
@@ -0,0 +1,19 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const test = require('tape');
+const fn = require('../lib');
+
+const file = join(__dirname, '.tmp', 'out.js');
+
+test('ReplaceText', t => {
+	t.equal(typeof fn, 'function', 'exports a function');
+	t.end();
+});
+
+test('Usage', t => {
+	const str = readFileSync(file, 'utf8');
+	t.true(/DEMO_FOO/g.test(str), 'respects `foo.js` exclusion (via config)');
+	t.true(/const FLAG/g.test(str), 'respects `node_module` exclusion (via config)');
+	t.false(/process.env.NODE_ENV/g.test(str), 'replaces all `process.env.NODE_ENV` occurrences (via config)');
+	t.end();
+});
